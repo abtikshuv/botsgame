5ee4a0b6bbbbb786633cb1c91a3cded8a2e15ffc
AB <ABTikshuv@gmail.com>
2021-03-11 10:36:46 +0200

    updated the time

--- bots/13/18.js
+++
@@ -1,6 +0,0 @@
-const { expose } = require('threads/worker');
-expose({runTurn:function (ID, D, Z, N, R, ZONES, DRONES, EXTRA) {try {
-this will fail
-return runTurn(ID, D, Z, N, R, ZONES, DRONES, EXTRA);}catch (e) {return {drones: [], extra: {}, error: e}}}})
-
-module.exports = { runTurn };
